7df997d362f7f9a5e58f522d95e20218086d3dc0
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2021-02-27 13:22:33 +0100

    initial commit

---
+++ .config/fish/fish_greeting.fish
@@ -0,0 +1,6 @@
+function fish_greeting
+	echo -e (uname -ro | awk '{print " \\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uptime -p | sed 's/^up //' | awk '{print " \\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uname -n | awk '{print " \\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
+	set_color normal
+end
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/.dotfiles/.config/fish ~/.config/fish
