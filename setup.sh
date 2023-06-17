728b7cc9a46f722c25f15953c5b6ca997fa75b46
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2023-06-17 23:24:29 +0200

    add lf config

--- setup.sh
+++ setup.sh
@@ -4,8 +4,10 @@ rm ~/.config/nvim
 rm ~/.config/fish
 rm ~/.config/kitty
 rm ~/.config/starship.toml
+rm ~/.config/lf
 
 ln -fs $PWD/nvim ~/.config/nvim
 ln -fs $PWD/fish ~/.config/fish
 ln -fs $PWD/kitty ~/.config/kitty
 ln -fs $PWD/starship.toml ~/.config/starship.toml
+ln -fs $PWD/lf ~/.config/lf
