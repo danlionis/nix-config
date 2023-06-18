16b5c8ab98e91b6ddeb7fd0d9e13d74369fea20b
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2023-06-18 22:34:04 +0200

    add beatysh

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -8,6 +8,7 @@ return {
             null_ls.setup({
                 sources = {
                     null_ls.builtins.formatting.black,
+                    null_ls.builtins.formatting.beautysh,
                     null_ls.builtins.diagnostics.mypy,
                     null_ls.builtins.diagnostics.ruff,
                     -- null_ls.builtins.completion.spell,
