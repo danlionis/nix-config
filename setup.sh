73475143224047c4b4102f8499d394a690421a58
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2023-06-18 22:34:25 +0200

    better handling of existing configurations

--- setup.sh
+++ setup.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
-rm ~/.config/nvim
-rm ~/.config/fish
-rm ~/.config/kitty
-rm ~/.config/starship.toml
-rm ~/.config/lf
+remove_existing() {
+    local path="$1"
+
+    if [[ -L "$path" ]]; then
+        # If the path is a symlink, remove it
+        rm "$path"
+        echo "Symlink removed: $path"
+    elif [[ -d "$path" ]]; then
+        # If the path is a directory, delete it recursively
+        # rm -r "$path"
+        mv "$path" "$path".old
+        echo "Directory backed up: $path -> $path.bak"
+    else
+        # If the path is neither a symlink nor a directory, display an error message
+        echo "Error: $path is not a symlink or directory"
+    fi
+}
+
+# remove_existing /tmp/.config/test
+remove_existing ~/.config/nvim
+remove_existing ~/.config/fish
+remove_existing ~/.config/kitty
+remove_existing ~/.config/starship.toml
+remove_existing ~/.config/lf
 
 ln -fs $PWD/nvim ~/.config/nvim
 ln -fs $PWD/fish ~/.config/fish
