0bfb42342b31f26050d7db13e2d8aa6f196f54c9
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2023-07-25 14:52:08 +0200

    feat: prettier formatter

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -10,6 +10,10 @@ return {
                     null_ls.builtins.formatting.black,
                     null_ls.builtins.formatting.beautysh,
                     null_ls.builtins.formatting.prettier,
+                    -- null_ls.builtins.formatting.prettier.with({
+                    --     filetypes = { "html", "json", "yaml", "typescript", "javascript" },
+                    -- }),
+                    null_ls.builtins.formatting.markdownlint,
                     null_ls.builtins.formatting.nixpkgs_fmt,
                     null_ls.builtins.diagnostics.mypy,
                     null_ls.builtins.diagnostics.ruff,
