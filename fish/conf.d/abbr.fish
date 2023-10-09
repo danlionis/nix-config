8f633c8c0ce56e1a6cf92eb673fc6babd001e41a
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2023-10-09 15:45:23 +0200

    add la abbr

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -9,6 +9,7 @@ if command -v exa > /dev/null
     abbr -a ls "exa"
     abbr -a ll "exa -l"
     abbr -a lll "exa -la"
+    abbr -a la "exa -la"
     abbr -a lt "exa -laT -I '.git|target|node_modules'"
 else
     abbr -a ll "ls -l"
