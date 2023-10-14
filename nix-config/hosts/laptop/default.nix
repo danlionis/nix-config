69efe4e5beb92cd0b3a29773134b37c82fab8791
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2023-10-14 15:40:38 +0200

    add athena to hosts

--- nix-config/hosts/laptop/default.nix
+++ nix-config/hosts/laptop/default.nix
@@ -25,6 +25,12 @@
   networking.hostName = "dan-laptop"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  networking.extraHosts =
+    ''
+      100.91.107.48 athena
+    '';
+
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
