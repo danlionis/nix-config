1c755a8147055505f2f5f5bde16978c7c39a3b5d
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2023-10-27 15:00:09 +0200

    fix command path

--- hypr/scripts/nuclear_xdph.sh
+++ hypr/scripts/nuclear_xdph.sh
@@ -4,6 +4,6 @@ sleep 1
 killall -e xdg-desktop-portal-hyprland
 killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-hyprland &
+xdg-desktop-portal-hyprland &
 sleep 2
-/usr/lib/xdg-desktop-portal &
+xdg-desktop-portal &
