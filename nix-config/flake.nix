ff8e28dfea9cc6110950ef286f09515b0551fb62
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2023-10-27 15:01:27 +0200

    remove unneeded deps,

--- nix-config/flake.nix
+++ nix-config/flake.nix
@@ -5,14 +5,14 @@
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    xremap.url = "github:xremap/nix-flake";
+    # # Home manager
+    # home-manager.url = "github:nix-community/home-manager/release-23.05";
+    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    #
+    # xremap.url = "github:xremap/nix-flake";
   };
 
-  outputs = { self, nixpkgs, home-manager, xremap, ... }@inputs:
+  outputs = { self, nixpkgs, ... }@inputs:
     let inherit (self) outputs; in {
       # NixOS configuration entrypoint
       # Available through 'nixos-rebuild --flake .#your-hostname'
@@ -31,16 +31,16 @@
         };
       };
 
-      # Standalone home-manager configuration entrypoint
-      # Available through 'home-manager --flake .#your-username@your-hostname'
-      homeConfigurations = {
-        # FIXME replace with your username@hostname
-        "dan@dan-laptop" = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
-          # > Our main home-manager configuration file <
-          modules = [ ./home-manager/home.nix ];
-        };
-      };
+      # # Standalone home-manager configuration entrypoint
+      # # Available through 'home-manager --flake .#your-username@your-hostname'
+      # homeConfigurations = {
+      #   # FIXME replace with your username@hostname
+      #   "dan@dan-laptop" = home-manager.lib.homeManagerConfiguration {
+      #     pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+      #     extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+      #     # > Our main home-manager configuration file <
+      #     modules = [ ./home-manager/home.nix ];
+      #   };
+      # };
     };
 }
