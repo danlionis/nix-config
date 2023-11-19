74b569d8c35b863c08115d31652121b545869d66
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2023-11-19 16:24:15 +0100

    disable power key

--- nix-config/hosts/laptop/default.nix
+++ nix-config/hosts/laptop/default.nix
@@ -306,4 +306,9 @@
   services.ratbagd.enable = true;
 
   services.blueman.enable = true;
+
+  services.logind.extraConfig = ''
+    # don’t shutdown when power button is short-pressed
+    HandlePowerKey=ignore
+  '';
 }
