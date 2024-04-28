3fe282e5b8280f198412ae83090be298ed22dd67
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-04-28 19:40:12 +0200

    nix: wrap sway-audio-idle-inhibit in infinite loop

--- nix-config/packages/sway-audio-idle-inhibit/default.nix
+++ nix-config/packages/sway-audio-idle-inhibit/default.nix
@@ -1,40 +1,16 @@
-{ lib
-, fetchFromGitHub
-, stdenv
-, meson
-, pkg-config
-, libpulseaudio
-, wayland-protocols
-, wayland
-, ninja
+{ callPackage
+, writeShellScriptBin
+
 }:
 
-stdenv.mkDerivation {
-  pname = "sway-audio-idle-inhibit";
-  version = "0.1.1";
+let
+  unwrapped = callPackage ./unwrapped.nix { };
+in
 
-  src = fetchFromGitHub {
-    owner = "ErikReider";
-    repo = "SwayAudioIdleInhibit";
-    rev = "c850bc4812216d03e05083c69aa05326a7fab9c7";
-    sha256 = "sha256-MKzyF5xY0uJ/UWewr8VFrK0y7ekvcWpMv/u9CHG14gs=";
-  };
-
-  nativeBuildInputs = [
-    meson
-    ninja
-    pkg-config
-  ];
-
-  buildInputs = [
-    libpulseaudio
-    wayland
-    wayland-protocols
-  ];
-
-  meta = with lib; {
-    description = "Inhibit swayidle when audio is played";
-    license = licenses.gpl3Only;
-    maintainers = [ ];
-  };
-}
+writeShellScriptBin "sway-audio-idle-inhibit" ''
+  while true
+  do
+  	${unwrapped}/bin/sway-audio-idle-inhibit
+  	sleep 1
+  done
+''
