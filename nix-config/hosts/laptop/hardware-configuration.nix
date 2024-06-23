8c8f7d045cea166f241890e71fc663b393d4ab79
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-06-23 15:29:43 +0200

    nix: disable swap

--- nix-config/hosts/laptop/hardware-configuration.nix
+++ nix-config/hosts/laptop/hardware-configuration.nix
@@ -28,10 +28,7 @@
       fsType = "vfat";
     };
 
-  swapDevices = [{
-    device = "/var/lib/swapfile";
-    size = 8 * 1024;
-  }];
+  swapDevices = lib.mkForce [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
