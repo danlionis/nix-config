b9b59b27a8f42a9dbe73cd045594ed479992a90e
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2024-09-15 21:25:46 +0200

    nix: fonts: add google fonts

--- nixos/modules/fonts.nix
+++ nixos/modules/fonts.nix
@@ -9,5 +9,13 @@
         "Meslo"
       ];
     })
+    (google-fonts.override {
+      fonts = [
+        "SUSE" # looks cool, but google-fonts in nixpkgs is out of date
+        "Inter"
+        "Oswald"
+        "Roboto"
+      ];
+    })
   ];
 }
