3ff30fec5236bad0236ce5a268d8686803083ca2
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2025-03-14 10:03:53 +0100

    nix: use glance.yml config file

--- nixos/hosts/kronos/guests/glance/default.nix
+++ nixos/hosts/kronos/guests/glance/default.nix
@@ -8,12 +8,21 @@
 let
   port = 58080;
   domain = config.globals.domains.start;
+  importYaml =
+    file:
+    builtins.fromJSON (
+      builtins.readFile (
+        pkgs.runCommandNoCC "converted-yaml.json" { } ''
+          ${pkgs.yj}/bin/yj < "${file}" > "$out"
+        ''
+      )
+    );
 in
 {
   services.glance = {
     enable = true;
     package = pkgs.unstable.glance;
-    settings = builtins.fromJSON (lib.readFile ./glance.json) // {
+    settings = (importYaml ./glance.yml) // {
       server.port = port;
     };
   };
