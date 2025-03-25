513c64c65623268c29c3dd367ff951a31438b2e7
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2025-03-25 09:24:43 +0100

    nix: enable printer autodiscovery

--- nixos/modules/printing.nix
+++ nixos/modules/printing.nix
@@ -1,6 +1,12 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   # Enable CUPS to print documents.
   services.printing.enable = true;
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
   services.printing.drivers = [
     pkgs.brlaser
     pkgs.brgenml1lpr
