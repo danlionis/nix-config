c7bdca0c57d073ec6699e05bef320ce5e854adfa
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2025-10-18 00:50:55 +0200

    rekey secrets

--- keys.nix
+++ keys.nix
@@ -1,5 +1,5 @@
 rec {
-  dan-desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJx4XvyBnsone/e2O6oGxuRQQNzZwlobeOWGAtSO3EN9";
+  dan-desktop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOXOCJeBh3kzaOMH5gwKiXASVPsvR0L819me5Kasdi7+";
   dan-mbp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG9P2/XJxMMoeS3rwpaJ0OXckAa6p//1sDsYmv2cKV/M"; # macbook
 
   kronos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIP+5lDeQUQLd9jriFQLqpWjSpkn1kAk2QfzSINut7cR";
