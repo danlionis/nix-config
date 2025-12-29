bb3c5a3e59772670bb390e90700eab6e32762813
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2025-12-29 11:50:49 +0100

    gaming: add proton-ge

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -20,6 +20,9 @@
     programs.steam = {
       enable = true;
       gamescopeSession.enable = true;
+      extraCompatPackages = with pkgs; [
+        proton-ge-bin
+      ];
     };
 
     programs.gamemode.enable = true;
