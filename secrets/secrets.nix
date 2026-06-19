8f01cdc5cce005886cca333708719c01d73518bf
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2026-06-19 20:30:21 +0200

    agenix: rekey

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -15,6 +15,6 @@ in
   "linkding/env".publicKeys = keys.users ++ [ keys.kronos ];
 
   "beszel/kronos".publicKeys = keys.users ++ [ keys.kronos ];
-  "beszel/dan-pc".publicKeys = keys.users;
-  "beszel/work".publicKeys = keys.users;
+  "beszel/dan-pc".publicKeys = keys.users ++ keys.dan-pc;
+  "beszel/work".publicKeys = keys.users ++ keys.work;
 }
