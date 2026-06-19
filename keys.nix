883e6a625d98506cb4197d2aed59f253de34db1a
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2026-06-19 11:41:27 +0200

    agenix: more robust key management

--- keys.nix
+++ keys.nix
@@ -1,15 +1,29 @@
 rec {
-  dan-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOXOCJeBh3kzaOMH5gwKiXASVPsvR0L819me5Kasdi7+";
+  dan-pc-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOXOCJeBh3kzaOMH5gwKiXASVPsvR0L819me5Kasdi7+";
+  dan-pc-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJogjY+bM9NDHMnN8Iq5oN7abewBGPdN46z8FGb2BRu3";
+
   dan-mbp = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG9P2/XJxMMoeS3rwpaJ0OXckAa6p//1sDsYmv2cKV/M"; # macbook
-  dan-pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJogjY+bM9NDHMnN8Iq5oN7abewBGPdN46z8FGb2BRu3";
 
   kronos = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIP+5lDeQUQLd9jriFQLqpWjSpkn1kAk2QfzSINut7cR";
 
   backup = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBv9iHztDz+28HUUBcwGPugwPAjuNtcXQOy+MzgNx2/x";
 
-  dan = [
-    dan-user
-    dan-pc
-    dan-mbp
+  work-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKRYMStjBWF3OHwYmsxom2k0zccaaJordvB0tm9Tl/dk";
+
+  users = [
+    dan-pc-user
   ];
+
+  dan-pc = [
+    dan-pc-host
+    dan-pc-user
+  ];
+
+  work = [
+    work-host
+  ];
+
+  servers = [ kronos ];
+
+  all = dan-pc ++ work ++ servers;
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,18 +2,18 @@ let
   keys = import ../keys.nix;
 in
 {
-  "restic/password".publicKeys = keys.dan ++ [ keys.kronos ];
-  "restic/paperless-b2-env".publicKeys = keys.dan ++ [ keys.kronos ];
-  "restic/ssh_key".publicKeys = keys.dan ++ [ keys.kronos ];
+  "restic/password".publicKeys = keys.users ++ keys.dan-pc ++ [ keys.kronos ];
+  "restic/paperless-b2-env".publicKeys = keys.users ++ keys.dan-pc ++ [ keys.kronos ];
+  "restic/ssh_key".publicKeys = keys.users ++ keys.dan-pc ++ [ keys.kronos ];
 
-  "CLOUDFLARE_DNS_API_TOKEN".publicKeys = keys.dan ++ [ keys.kronos ];
+  "CLOUDFLARE_DNS_API_TOKEN".publicKeys = keys.users ++ [ keys.kronos ];
 
-  "password-hash".publicKeys = keys.dan ++ [ keys.kronos ];
+  "password-hash".publicKeys = keys.all;
 
-  "pocket-id/encryption_key".publicKeys = keys.dan ++ [ keys.kronos ];
+  "pocket-id/encryption_key".publicKeys = keys.users ++ [ keys.kronos ];
 
-  "linkding/env".publicKeys = keys.dan ++ [ keys.kronos ];
+  "linkding/env".publicKeys = keys.users ++ [ keys.kronos ];
 
-  "beszel/kronos".publicKeys = keys.dan ++ [ keys.kronos ];
-  "beszel/dan-pc".publicKeys = [ keys.dan-pc ];
+  "beszel/kronos".publicKeys = keys.users ++ [ keys.kronos ];
+  "beszel/dan-pc".publicKeys = keys.users ++ [ keys.dan-pc ];
 }
